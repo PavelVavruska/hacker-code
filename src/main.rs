5cc4c02f6ff04b77ff65ea14d83462d57ba88f6c
rust <vavruska.pavel@gmail.com>
2022-04-27 20:38:49 +0200

    feat: more tests for socks, boilercode for hackerrank-ish parsing of std input.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+use std::collections::HashMap;
 use std::io::{stdin, Read};
 
-pub mod questions;
+mod questions;
 
+fn parse_vec_int_from_str(first_line: Option<&str>) -> Vec<u32> {
+    return first_line
+        .unwrap_or("")
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|x| x.parse::<u32>().ok().expect("Parse error."))
+        .collect();
+}
 fn main() {
     let mut buf = String::new();
-    stdin().read_to_string(&mut buf).expect("Error.");
-    for ch in buf.chars() {
-        println!("{}", ch);
+    stdin().read_to_string(&mut buf).expect("Parse error.");
+
+    let mut str_iterator = buf.lines();
+    //let _zero = str_iterator.next(); // skip first line
+    let array_from_str = parse_vec_int_from_str(str_iterator.next());
+
+    let mut hashmap = HashMap::new();
+    for color in array_from_str {
+        *hashmap.entry(color).or_insert(0) += 1;
     }
+    let mut count = 0;
+    for (&_key, &val) in hashmap.iter() {
+        if val < 2 {
+            continue;
+        }
+        count += val / 2;
+    }
+    println!("{}", count);
 }
--- src/questions/socks.rs
+++ src/questions/socks.rs
@@ -15,9 +15,28 @@ pub fn get_number_of_pairs(_number: i8, color_array: &[i8]) -> i8 {
     return count;
 }
 
-#[test]
-fn test_number_of_pairs() {
-    let input_array = [1, 2, 1, 2, 1, 3, 2];
-    let amount = 7;
-    assert_eq!(2, get_number_of_pairs(amount, &input_array));
+#[cfg(test)]
+mod tests {
+    use super::get_number_of_pairs;
+
+    #[test]
+    fn test_number_of_pairs_1() {
+        let input_array = [1, 2, 1, 2, 1, 3, 2];
+        let amount = 7;
+        assert_eq!(2, get_number_of_pairs(amount, &input_array));
+    }
+
+    #[test]
+    fn test_number_of_pairs_2() {
+        let input_array = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+        let amount = 9;
+        assert_eq!(3, get_number_of_pairs(amount, &input_array));
+    }
+
+    #[test]
+    fn test_number_of_pairs_3() {
+        let input_array = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3];
+        let amount = 10;
+        assert_eq!(4, get_number_of_pairs(amount, &input_array));
+    }
 }
